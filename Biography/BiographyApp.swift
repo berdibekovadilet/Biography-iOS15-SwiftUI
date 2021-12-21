9b1e0ca1cca731c6893ba8e22c9cf3bf27aeb39b
Adilet <berdibekovadilet@icloud.com>
2021-12-21 12:58:18 +0300

    Initial Commit

---
+++ Biography/BiographyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiographyApp.swift
+//  Biography
+//
+//  Created by Adilet Berdibekov on 21.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct BiographyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
